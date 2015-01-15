b9b159d23a21ac514e8ff55a322dfae17b9ba38b
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-15 23:11:56 +0300

    Changed name of custom validator

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: { message: "%{value} не может быть пустым" }
-  validates_with TextsEqualValidator
+  validates_with TextsEqualityValidator
 end
