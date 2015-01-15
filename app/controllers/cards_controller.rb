5c76db8a64adba8e0c5503bad7a3daf2579af568
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-15 13:13:23 +0300

    Added page for editing existing cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,23 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to cards_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
