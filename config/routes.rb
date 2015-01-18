10f1ded4adc4845d60a7a05cff8ff6be3c095770
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-18 14:25:20 +0300

    Refactoring code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :cards
-  post "/check", to: "cards#check", as: "check_card"
+  post "check", to: "cards#check", as: "check_card"
 
 # The priority is based upon order of creation: first created -> highest priority.
 # See how all your routes lay out with "rake routes".
