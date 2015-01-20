75e9ea50c694c8782d46b28b6128c562f5872e5d
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-21 02:03:05 +0300

    Corrected style flaws

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe Card do
-  subject(:card) { Card.new(original_text:"Goodbye", translated_text: "Пока", review_date: Date.today) }
-  let(:card_phrase) { Card.new(original_text:"Good morning", translated_text: "Доброе утро", review_date: Date.today) }
+  subject :card { Card.new(original_text: "Goodbye", translated_text: "Пока", review_date: Date.today) }
+  let :card_phrase { Card.new(original_text: "Good morning", translated_text: "Доброе утро", review_date: Date.today) }
 
   it { is_expected.to respond_to(:check_translation).with(1).argument }
   it { is_expected.not_to respond_to(:check_translation).with(0).arguments }
 
   context "#check_translation" do
     context "expect return true when user wrote" do
-
       it "a lowercase right word" do
         user_input = "пока"
         expect(card.check_translation(user_input)).to be == true
