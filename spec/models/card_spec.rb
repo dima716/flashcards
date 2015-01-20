192d3aebdd2461d81b36c5269e98ee60917c0946
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-21 02:07:41 +0300

    Added brackets to methods in card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Card do
-  subject :card { Card.new(original_text: "Goodbye", translated_text: "Пока", review_date: Date.today) }
-  let :card_phrase { Card.new(original_text: "Good morning", translated_text: "Доброе утро", review_date: Date.today) }
+  subject(:card) { Card.new(original_text: "Goodbye", translated_text: "Пока", review_date: Date.today) }
+  let(:card_phrase) { Card.new(original_text: "Good morning", translated_text: "Доброе утро", review_date: Date.today) }
 
   it { is_expected.to respond_to(:check_translation).with(1).argument }
   it { is_expected.not_to respond_to(:check_translation).with(0).arguments }
