c6ffd62959f05ed2200061e891ed306df51a9f5a
Anikeychik Dmitry <Dima716@yandex.ru>
2015-01-28 13:24:25 +0300

    Changed user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,6 @@ FactoryGirl.define do
     email "MyString"
     password "MyString"
     password_confirmation "MyString"
-    salt "asdasdastr4325234324sdfds"
-    crypted_password Sorcery::CryptoProviders::BCrypt.encrypt("secret", "asdasdastr4325234324sdfds")
     profile_image_url "MyString"
   end
 end
