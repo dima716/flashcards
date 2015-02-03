6f288709c5c54cbb75f31343dd556ff5970200c4
Anikeychik Dmitry <Dima716@yandex.ru>
2015-02-04 00:12:02 +0300

    Rolled back redirect changes to make integration test pass

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,10 @@ class HomeController < ApplicationController
   def index
     if current_deck = current_user.current_deck
       @cards = current_deck.cards
-      unless @card = @cards.for_review.first
-        flash[:reviewed] = "Все карточки повторены"
-        redirect_to decks_path
-      end
+      flash[:reviewed] = "Все карточки повторены" unless @card = @cards.for_review.first
     elsif current_user.cards.present?
       @cards = current_user.cards
-      unless @card = @cards.for_review.first
-        flash[:reviewed] = "Все карточки повторены"
-        redirect_to decks_path
-      end
+      flash[:reviewed] = "Все карточки повторены" unless @card = @cards.for_review.first
     else
       flash[:empty] = "Для начала упражнений необходимо добавить карточки"
       redirect_to decks_path
