2cf68ed8ae4adf4613c783d70c718c4582a92ac0
Anikeychik Dmitry <Dima716@yandex.ru>
2015-02-04 00:06:14 +0300

    Changed card border and redirect when all card reviewed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,16 @@ class HomeController < ApplicationController
   def index
     if current_deck = current_user.current_deck
       @cards = current_deck.cards
-      flash[:reviewed] = "Все карточки повторены" unless @card = @cards.for_review.first
+      unless @card = @cards.for_review.first
+        flash[:reviewed] = "Все карточки повторены"
+        redirect_to decks_path
+      end
     elsif current_user.cards.present?
       @cards = current_user.cards
-      flash[:reviewed] = "Все карточки повторены" unless @card = @cards.for_review.first
+      unless @card = @cards.for_review.first
+        flash[:reviewed] = "Все карточки повторены"
+        redirect_to decks_path
+      end
     else
       flash[:empty] = "Для начала упражнений необходимо добавить карточки"
       redirect_to decks_path
